9d365d51dedb7d17aa74e74f3d3c6167f4766a86
praneeth-coder-2005 <savediskofc@gmail.com>
2024-12-23 21:46:46 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+TELEGRAM_BOT_TOKEN = "7974031259:AAG2hVunyhQZsLXS44TROPKbjmruuwBLxDY"  # Replace with your token
+WATERMARK_IMAGE = "https://envs.sh/Rdy.jpg" # Replace if you are using image watermark
+WATERMARK_TEXT = "@ClawMoviez" # Replace with your watermark text
+TEMP_DIR = "temp_files"  # For temporary file storage
