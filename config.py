89e156eb588bfd4f41ec7418c31fec25ecc9104b
praneeth-coder-2005 <savediskofc@gmail.com>
2024-12-23 22:48:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ TELEGRAM_BOT_TOKEN = "7974031259:AAG2hVunyhQZsLXS44TROPKbjmruuwBLxDY"  # Replace
 WATERMARK_IMAGE = "https://envs.sh/Rdy.jpg" # Replace if you are using image watermark
 WATERMARK_TEXT = "@ClawMoviez" # Replace with your watermark text
 TEMP_DIR = "temp_files"  # For temporary file storage
-TELEGRAM_API_ID = "22250562"
-TELEGRAM_API_HASH = "07754d3bdc27193318ae5f6e6c8016af"
+TELEGRAM_API_ID = 22250562
+TELEGRAM_API_HASH = 07754d3bdc27193318ae5f6e6c8016af
