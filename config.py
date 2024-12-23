4aa7d886e4624aa26f7fec98e74dba2d84fe9f22
praneeth-coder-2005 <savediskofc@gmail.com>
2024-12-23 22:56:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ TELEGRAM_BOT_TOKEN = "7974031259:AAG2hVunyhQZsLXS44TROPKbjmruuwBLxDY"  # Replace
 TEMP_DIR = "temp_files"  # For temporary file storage
 
 #  Api id, and Api hash obtained from my.telegram.org
-#  must set as enviroment variables,
-#  not necessary to set here.
+#  must set as integers and strings respectively
+#  not necessary to set as enviroment variables
 
-# TELEGRAM_API_ID = "YOUR_TELEGRAM_API_ID"
-# TELEGRAM_API_HASH = "YOUR_TELEGRAM_API_HASH"
+TELEGRAM_API_ID = 22250562 # Replace with your api id
+TELEGRAM_API_HASH = "07754d3bdc27193318ae5f6e6c8016af"  # Replace with your api hash
