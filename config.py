be81aaab5ede0c5ce8361bea53c350270786bead
praneeth-coder-2005 <savediskofc@gmail.com>
2024-12-23 22:54:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 # config.py
-TELEGRAM_BOT_TOKEN = "7974031259:AAG2hVunyhQZsLXS44TROPKbjmruuwBLxDY"  # Replace with your token
-WATERMARK_IMAGE = "https://envs.sh/Rdy.jpg" # Replace if you are using image watermark
-WATERMARK_TEXT = "@ClawMoviez" # Replace with your watermark text
+
+TELEGRAM_BOT_TOKEN = "7974031259:AAG2hVunyhQZsLXS44TROPKbjmruuwBLxDY"  # Replace with your bot token
 TEMP_DIR = "temp_files"  # For temporary file storage
-TELEGRAM_API_ID = 22250562
-TELEGRAM_API_HASH = 07754d3bdc27193318ae5f6e6c8016af
+
+#  Api id, and Api hash obtained from my.telegram.org
+#  must set as enviroment variables,
+#  not necessary to set here.
+
+# TELEGRAM_API_ID = "YOUR_TELEGRAM_API_ID"
+# TELEGRAM_API_HASH = "YOUR_TELEGRAM_API_HASH"
